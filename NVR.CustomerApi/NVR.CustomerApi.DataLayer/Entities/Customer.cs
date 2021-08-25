507017659e05e6f1600bb09372f82893bcd8c126
jlafond <j.lafond55@gmail.com>
2021-08-25 19:43:44 -0400

    added CustomerEntity class in DataLayer

---
+++ NVR.CustomerApi/NVR.CustomerApi.DataLayer/Entities/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NVR.CustomerApi.DataLayer.Entities
+{
+    public class Customer
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
