f34b6a929ef3bcb5bb98d39ad29a007000c89edf
jlafond <j.lafond55@gmail.com>
2021-08-25 19:28:07 -0400

    Add CustomerController with Get and Post endpoints

---
+++ NVR.CustomerApi/NVR.CustomerApi.Api/Controllers/CustomerController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NVR.CustomerApi.Api.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public CustomerController()
+        {
+
+        }
+
+        [HttpGet]
+        public IActionResult GetCustomers()
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult AddCustomer()
+        {
+            return Ok();
+        }
+
+
+    }
+}
