432bbb7f88c1d3840b9f3e20e2b7524760940468
jlafond <j.lafond55@gmail.com>
2021-08-25 19:33:43 -0400

    Create CustomerService and ICustomerService with basic Get and Save methods

---
+++ NVR.CustomerApi/NVR.CustomerApi.Services/Services/CustomerService.cs
@@ -0,0 +1,22 @@
+﻿using NVR.CustomerApi.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NVR.CustomerApi.Services.Services
+{
+    public class CustomerService : ICustomerService
+    {
+        public CustomerService() { }
+
+        public object GetCustomers()
+        {
+            return new object[1];
+        }
+
+        public bool SaveCustomer()
+        {
+            return true;
+        }
+    }
+}
